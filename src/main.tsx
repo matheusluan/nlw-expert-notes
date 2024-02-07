c3280e6a9f501464dea501b8e7de1ec2635187aa
matheus <ti01@muffatao.com.br>
2024-02-07 10:31:31 -0300

    teste

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,6 @@ import { Toaster } from 'sonner'
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
-    <Toaster richColors />
+    <Toaster richColors  />
   </React.StrictMode>,
 )
